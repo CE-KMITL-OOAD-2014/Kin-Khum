9426b3502939ea27ab691be74d690a7416ed6173
warittha <catsan_merlin_23july1989@hotmail.com>
2014-11-15 05:42:42 +0700

    show&review

--- kinkhum/app/views/show.blade.php
+++ kinkhum/app/views/show.blade.php
@@ -14,32 +14,66 @@
 
 	<div class="container">
 
-	<div class="page-header">
-		<h1>Restaurant</h1>
-	</div>
-	
-
-		<div container >
-			
-				<img src="a.png"  class="img-thumbnail" >
-
-			<hr>
-	
-								<p > <FONT COLOR=#0000FF>{{$show->getRestaurant()}} &nbsp; {{$show->getType()}}<br></FONT>
-				
-				
-								</p>
-				
-			<hr>
-							 
-		<h1>Review<small>Reataurant</small>></h1>
-			
-
+		<div class="page-header">
+			<h1>Restaurant</h1>
 		</div>
 
 
+		<div container >
+			
+			<img src="...." na class="img-thumbnail" >
+
+			<hr>
+
+			<p > <FONT COLOR=#0000FF>{{$show->getRestaurant()}} &nbsp; {{$show->getType()}} &nbsp; {{$show->getDetail()}}<br></FONT>
+				
+				
+			</p>
+
+			<hr>
+
+			<h3>Review <small>Restaurant</small></h3>
+
+			<!-- <form action = "{{ url('res/review') }}"  method="post" enctype="multipart/form-data"> -->
+
+				<form role="form">
+					<div class="form-group">
+
+
+						<label for="Create_reataurant">Detail</label><br>
+						<textarea class="form-control" rows="3" placeholder="detail"></textarea>
+
+						<br>
+						<label class="radio-inline">
+							<input type="radio" name="inlineRadioOptions" id="inlineRadio1" value="1"> 1
+						</label>
+						<label class="radio-inline">
+							<input type="radio" name="inlineRadioOptions" id="inlineRadio2" value="2"> 2
+						</label>
+						<label class="radio-inline">
+							<input type="radio" name="inlineRadioOptions" id="inlineRadio3" value="3"> 3
+						</label>
+
+						<label class="radio-inline">
+							<input type="radio" name="inlineRadioOptions" id="inlineRadio1" value="4"> 4
+						</label>
+
+
+						<label class="radio-inline">
+							<input type="radio" name="inlineRadioOptions" id="inlineRadio1" value="5"> 5
+						</label>
+						<br>
+						<input type="submit" value="create"<br>
+
+					</div>
+				</form>
+
+
+			</div>
+
+
+		</div>
 	</div>
-</div>
 </body>
 
 </html>
