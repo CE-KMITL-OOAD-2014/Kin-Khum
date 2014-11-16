65d5fc7ea751220d088d9b911749a60e874e6524
warittha <catsan_merlin_23july1989@hotmail.com>
2014-11-16 17:21:54 +0700

    final

--- kinkhum/app/views/loginss.blade.php
+++ kinkhum/app/views/loginss.blade.php
@@ -3,12 +3,17 @@
 
 
 
-<h2> LOGIN </h2>
+ <div class="page-header">
+  <h2>LOGIN</h2>
+  </div>
+
+<div class="comtainer">
 <form  method="post" action="/login">
 
 <input type="text" name="username" placeholder="Username">
 <input type="password" name="password" placeholder="Password">
 <input type="submit" value="login"<br>
 </form>
+</div>
 
 @stop
